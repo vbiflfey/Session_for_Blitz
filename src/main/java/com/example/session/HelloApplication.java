a45aa1f4e20444a38f7fc7b8570a84ff0e900f70
vbiflfey <comandor.31032003@gmail.com>
2023-09-29 01:34:29 +0300

    Created jar file and add steel. Enjoy my brother

--- src/main/java/com/example/session/HelloApplication.java
+++ src/main/java/com/example/session/HelloApplication.java
@@ -3,16 +3,19 @@ package com.example.session;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 223, 253);
+        Scene scene = new Scene(fxmlLoader.load(),  223, 253);
+        stage.getIcons().add(new Image("icon.png"));
         stage.setTitle("Сессионка");
         stage.setResizable(false);
         stage.setAlwaysOnTop(true);
